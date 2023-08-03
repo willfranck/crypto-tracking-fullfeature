2e2c5e57cbf788f5cac25fcdf74e0b0ad314c315
willfranck <43352447+willfranck@users.noreply.github.com>
2023-08-03 16:06:44 -0400

    Build: Test basic signup page and path routes

--- src/app/pages/signup/page.tsx
+++ src/app/pages/signup/page.tsx
@@ -1,7 +1,27 @@
 'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import axios from 'axios'
+import styles from '@styles/page.module.css'
+
 
 export default function SignUp() {
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  })
+
+  const onSignUp = async () => {
+
+  }
+
+
+
+
   return (
-    <h1>Sign Up</h1>
+    <div className={styles.center}>
+      <h1>Sign Up</h1>
+    </div>
   )
 }
