17fe17f7b78f8eef5521082bb24e0baa33c22ae1
willfranck <43352447+willfranck@users.noreply.github.com>
2023-08-04 12:12:08 -0400

    Build: Created Mongoose user schema

---
+++ src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Provide your username'],
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: [true, 'Enter your password']
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date
+})
+
+const User = mongoose.models.users || mongoose.model('User', userSchema)
+
+export default User
