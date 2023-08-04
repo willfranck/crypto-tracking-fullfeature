3b8bac402fedc7ccd5de55314f41fd191b5813eb
willfranck <43352447+willfranck@users.noreply.github.com>
2023-08-04 11:49:11 -0400

    Fix: Formatting on Dashboard page code

--- src/app/pages/dashboard/[id]/page.tsx
+++ src/app/pages/dashboard/[id]/page.tsx
@@ -3,9 +3,9 @@ import styles from '@styles/page.module.css'
 export default function UserProfile({params}: any) {
   return (
     <section className={styles.center}>
-        <h1>Dashboard</h1>
-        <p>{params.id}</p>
-        <p>Saved Coins</p>
+      <h1>Dashboard</h1>
+      <p>{params.id}</p>
+      <p>Saved Coins</p>
     </section>
   )
 }
--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -3,8 +3,8 @@ import styles from '@styles/page.module.css'
 export default function Dashboard() {
   return (
     <section className={styles.center}>
-        <h1>Dashboard</h1>
-        <p>Saved Coins</p>
+      <h1>Dashboard</h1>
+      <p>Saved Coins</p>
     </section>
   )
 }
