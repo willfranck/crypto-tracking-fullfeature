91e5244387dbfac6d5a70a79b1de04ef021b4e8d
willfranck <43352447+willfranck@users.noreply.github.com>
2023-09-21 14:33:05 -0400

    Build: Created separate component for dashboard menu

---
+++ src/app/components/dashMenu.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import buttons from '@utils/buttons'
+import { DashMenuBtns, Homepage } from '@components/dashMenuButtons'
+
+interface MenuBtns {
+  key: number,
+  datatype: string,
+  label: string,
+  aria: string,
+  active: boolean,
+}
+
+interface DashMenuProps {
+  activeDatatypeChange: (datatype: string) => void,
+}
+
+
+export default function DashMenu({activeDatatypeChange}: DashMenuProps) {
+  const [dashMenuBtns, setDashMenuBtns] = useState<MenuBtns[]>([])
+
+  useEffect(() => {
+    function getDashMenuBtns() {
+      const btnData = buttons.dashBtns
+      setDashMenuBtns(btnData)
+    }
+
+    getDashMenuBtns()
+  }, [])
+
+  function handleActiveBtn(key: number) {
+    const updatedButtons = buttons.dashBtns.map((button) => ({
+      ...button,
+      active: button.key === key,
+    }))
+
+    setDashMenuBtns(updatedButtons)
+
+    const activeBtn = updatedButtons.find((button) => button.active);
+    if (activeBtn) {
+      activeDatatypeChange(activeBtn.datatype);
+    }
+  }
+
+
+  return (
+    <aside className='sticky top-0 flex justify-center items-start w-full lg:w-60 min-h-full my-6 md:px-6 bg-black border-b-2 lg:border-b-0 lg:border-r-2 border-slate-800'>
+      <ul className='flex lg:flex-col justify-center items-center w-full'>
+        <li className='flex justify-center items-center w-2/3 h-20 text-slate-400'>
+          <h3>MENU</h3>
+        </li>
+
+        {dashMenuBtns && 
+          dashMenuBtns.map((btnProps) => (
+            <li className='flex justify-center items-center w-2/3 h-20'>
+              <DashMenuBtns 
+                key={btnProps.key}
+                datatype={btnProps.datatype}
+                label={btnProps.label}
+                aria={btnProps.aria}
+                active={btnProps.active}
+                setActive={() => handleActiveBtn(btnProps.key)}
+              />
+            </li>
+          ))
+        }
+
+        <li className='flex justify-center items-center w-2/3 h-20 text-slate-400'>
+          <Homepage />
+        </li>
+      </ul>
+    </aside>
+  )
+} 
--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,55 +1,19 @@
 'use client'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
-import buttons from '@utils/buttons'
 import NavBar from '@components/navBar'
-import { DashMenuBtns, Homepage } from '@components/dashMenuButtons'
-import SavedCoins from '@components/savedCoins'
+import DashMenu from '@components/dashMenu'
+import SavedCoins from '@components/dashSavedCoins'
 import CryptoCardGrid from '@components/cryptoCardGrid'
-
-interface MenuBtns {
-  key: number,
-  datatype: string,
-  label: string,
-  aria: string,
-  active: boolean,
-}
 
 
 export default function UserProfile() {
   const { data: session, status } = useSession()
-  const [dashMenuBtns, setDashMenuBtns] = useState<MenuBtns[]>([])
-  const [activeDatatype, setActiveDatatype] = useState<string>('')
+  const [activeDatatype, setActiveDatatype] = useState<string>('saved-coins')
 
-  useEffect(() => {
-    function getDashMenuBtns() {
-      const btnData = buttons.dashBtns
-      setDashMenuBtns(btnData)
-
-      const activeBtn = btnData.find((button) => button.active)
-      if (activeBtn) {
-        setActiveDatatype(activeBtn.datatype)
-      }
-    }
-
-    getDashMenuBtns()
-  }, [])
-
-
-  function handleActiveBtn(key: number) {
-    const updatedButtons = buttons.dashBtns.map((button) => ({
-      ...button,
-      active: button.key === key,
-    }))
-
-    buttons.dashBtns = updatedButtons
-    setDashMenuBtns(updatedButtons)
-    
-    const activeBtn = updatedButtons.find((button) => button.active)
-    if (activeBtn) {
-      setActiveDatatype(activeBtn.datatype)
-    }
+  const handleActiveDatatypeChange = (datatype: string) => {
+    setActiveDatatype(datatype)
   }
 
 
@@ -62,36 +26,13 @@ export default function UserProfile() {
         <NavBar />
 
         <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-          <aside className='sticky top-0 flex justify-center items-start w-full lg:w-60 min-h-full my-6 md:px-6 bg-black border-b-2 lg:border-b-0 lg:border-r-2 border-slate-800'>
-            <ul className='flex lg:flex-col justify-center items-center w-full'>
-              <li className='flex justify-center items-center w-2/3 h-20 text-slate-400'>
-                <h3>MENU</h3>
-              </li>
-
-              {dashMenuBtns && 
-                dashMenuBtns.map((btnProps) => (
-                  <li className='flex justify-center items-center w-2/3 h-20'>
-                    <DashMenuBtns 
-                      key={btnProps.key}
-                      datatype={btnProps.datatype}
-                      label={btnProps.label}
-                      aria={btnProps.aria}
-                      active={btnProps.active}
-                      setActive={() => handleActiveBtn(btnProps.key)}
-                    />
-                  </li>
-                ))
-              }
-
-              <li className='flex justify-center items-center w-2/3 h-20 text-slate-400'>
-                <Homepage />
-              </li>
-            </ul>
-          </aside>
+          <DashMenu 
+            activeDatatypeChange={handleActiveDatatypeChange}
+          />
 
           <article className='flex flex-1 w-full xl:max-w-full'>
             {activeDatatype === 'saved-coins' ? (
-                <SavedCoins />
+              <SavedCoins />
 
               ) : activeDatatype === 'search-coins' ? (
                 <CryptoCardGrid />
