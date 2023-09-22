784f3d3db738e9b5270824a2164a9dc6395bf365
willfranck <43352447+willfranck@users.noreply.github.com>
2023-09-22 13:08:38 -0400

    Added styling to hero coins

--- src/app/components/heroCard.tsx
+++ src/app/components/heroCard.tsx
@@ -32,7 +32,7 @@ export default function HeroCard() {
         </div>
       </article>
 
-      <article className='flex flex-col xl:flex-row w-11/12 xl:w-5/6 px-4 rounded-3xl bg-slate-900'>
+      <article className='flex flex-col xl:flex-row w-11/12 xl:w-5/6 px-4 translate-y-1/2 rounded-3xl bg-slate-900'>
         <HeroCoins />
       </article>
     </section>
