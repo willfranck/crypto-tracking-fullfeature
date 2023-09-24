27d47d098fdcdabb73c38bbea7a088ca606e647d
willfranck <43352447+willfranck@users.noreply.github.com>
2023-09-24 18:25:12 -0400

    Added container flex div for crypto grid

--- src/app/components/cryptoCardGrid.tsx
+++ src/app/components/cryptoCardGrid.tsx
@@ -101,6 +101,7 @@ export default function CryptoCardGrid() {
         </form>
       </div>
     
+      <div className='flex justify-center items-center'>
       <div className='grid grid-cols-1 md:grid-cols-[minmax(256px,512px)_minmax(256px,512px)] gap-x-8 gap-y-4 w-full rounded-2xl bg-slate-900'>
         {slicedCurrencies &&
           slicedCurrencies.map((coin) => (
@@ -115,6 +116,7 @@ export default function CryptoCardGrid() {
             </div>
           ))
         }
+      </div>
       </div>
     </div>
   )
