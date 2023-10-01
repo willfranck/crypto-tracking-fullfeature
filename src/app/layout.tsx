f6dea2d0fb4927a6f75db632f6392c2295551ef7
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-01 04:17:01 -0400

    Fix: Put full height back on html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='h-full'>
       <body 
         className={poppins.className + ' h-full'}
         suppressHydrationWarning={true}
