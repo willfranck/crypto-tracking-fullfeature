4e1c5c5c0c61277b94963e8341dbf647ca1f8a70
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-04 21:03:29 -0400

    Fix: Added preventDefault to search bar

--- src/app/components/cryptoCardGrid.tsx
+++ src/app/components/cryptoCardGrid.tsx
@@ -77,6 +77,7 @@ export default function CryptoCardGrid() {
             placeholder='Name | Symbol'
             value={coinSearch}
             onChange={handleCoinSearch}
+            onSubmit={(e) => e.preventDefault}
             className='w-full rounded-md border-0 px-3 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           ></input>
         </form>
