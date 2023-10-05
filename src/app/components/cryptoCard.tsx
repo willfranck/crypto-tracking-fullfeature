db31b65e156dbde123bbcba47b9ec6f301772917
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-05 16:09:01 -0400

    Feat: Trying v2 of dynamic rounding for smaller currencies

--- src/app/components/cryptoCard.tsx
+++ src/app/components/cryptoCard.tsx
@@ -20,6 +20,15 @@ export default function CryptoCard(cryptoProps: Coin) {
   const [updatingUserCoins, setUpdatingUserCoins] = useState(false)
   const userSavedCoins = cryptoProps.savedCoins || []
 
+  const dynamicRounding = (price: number) => {
+    if (Math.abs(price) < 1) {
+      const decimalPlaces = Math.max(2, Math.ceil(Math.log10(1 / Math.abs(price))));
+      return decimalPlaces
+    } else {
+      return 2;
+    }
+  }
+
   async function handleSubmit(symbol: string) {
     try {
       setUpdatingUserCoins(true)
@@ -52,7 +61,8 @@ export default function CryptoCard(cryptoProps: Coin) {
           <span className='text-gray-300'>{cryptoProps.symbol}</span>
         </div>
         
-        <h3 className='text-green-400'>${(Math.round(100 * cryptoProps.price) / 100).toFixed(2)}</h3>
+        <h3 className='text-green-400'>${Number(cryptoProps.price).toFixed(dynamicRounding(cryptoProps.price))}</h3>
+        {/* <h3 className='text-green-400'>${(Math.round(100 * cryptoProps.price) / 100).toFixed(dynamicRounding)}</h3> */}
       </div>
       
       <div className='flex justify-between w-full'>
