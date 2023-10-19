453e77cdd50d5ae7f9cd2487ba1ed7852c7eb24f
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-19 13:13:01 -0400

    Trying getServerSession in dashboard page | Returned middleware to original code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,17 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@lib/auth'
+import { redirect } from 'next/navigation'
 import NavBar from '@components/navBar'
 import ClientComponents from '@components/dashClientComponents'
 
 
 export default function UserProfile() {
+  const session = getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/signin')
+  }
+
   return (
     <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
       <NavBar />
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,4 @@
-// import { getServerSession } from 'next-auth'
-// import { authOptions } from '@lib/auth'
-
-
 export { default } from 'next-auth/middleware'
 
 export const config = { matcher: '/dashboard' }
 
-// export async function getServerSideProps(context: any) {
-//   const session = await getServerSession(
-//     context.req,
-//     context.res,
-//     authOptions
-//   )
-
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: '/signin',
-//         permanent: false,
-//       }
-//     }
-//   }
-
-//   return {
-//     props: {
-//       session,
-//     }
-//   }
-// }
