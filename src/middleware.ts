7937f915e86d86556ce1669426672b49c8c2bead
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-19 12:43:22 -0400

    Removed new function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@lib/auth'
+// import { getServerSession } from 'next-auth'
+// import { authOptions } from '@lib/auth'
 
+
+export { default } from 'next-auth/middleware'
 
 export const config = { matcher: '/dashboard' }
 
-export async function getServerSideProps(context: any) {
-  const session = await getServerSession(
-    context.req,
-    context.res,
-    authOptions
-  )
+// export async function getServerSideProps(context: any) {
+//   const session = await getServerSession(
+//     context.req,
+//     context.res,
+//     authOptions
+//   )
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/signin',
-        permanent: false,
-      }
-    }
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/signin',
+//         permanent: false,
+//       }
+//     }
+//   }
 
-  return {
-    props: {
-      session,
-    }
-  }
-}
-
-export { default } from 'next-auth/middleware'
+//   return {
+//     props: {
+//       session,
+//     }
+//   }
+// }
