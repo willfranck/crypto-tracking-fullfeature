46666b5583c4d4d62045590da2f5af5a02fd1366
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-19 11:54:39 -0400

    Added pages to middleware to match authOptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
-import { withAuth } from 'next-auth/middleware'
+import withAuth from "next-auth/middleware"
+import { authOptions } from '@lib/auth'
 
 export const config = { matcher: '/dashboard' }
 
 export default withAuth({
   pages: {
-    signIn: '/login'
-  }
+    signIn: '/signin',
+  },
+  
+  jwt: { decode: authOptions.jwt?.decode },
+  callbacks: {
+    authorized: ({ token }) => !!token,
+  },
 })
