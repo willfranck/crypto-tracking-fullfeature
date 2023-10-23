45953c512b49e58916c3563c17dbfaf7ca2d23ac
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-23 17:41:38 -0400

    Fix: Added useEffect interval polling of session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 import { redirect } from 'next/navigation'
 import NavBar from '@components/navBar'
 import ClientComponents from '@components/dashClientComponents'
@@ -7,9 +9,16 @@ import ClientComponents from '@components/dashClientComponents'
 export default function UserProfile() {
   const { data: session } = useSession()
 
-  if(!session) {
-    return redirect('/signin')
-  } 
+  useEffect(() => {
+    const interval = setInterval(() => session, 1000 * 60)
+
+    if(!session) {
+      redirect('/signin')
+    }
+
+    return clearInterval(interval)
+  }, [session])
+
   
   return (
     <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
