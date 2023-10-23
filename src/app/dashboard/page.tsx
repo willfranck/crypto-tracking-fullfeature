353492a3f294422c957f35eed9496e6c5983f626
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-23 14:54:46 -0400

    Fix: Added else statement to dashboard return

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,15 +10,16 @@ export default function UserProfile() {
 
   if (!session) {
     redirect('/signin')
+  
+  } else {
+    return (
+      <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
+        <NavBar />
+
+        <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
+          <ClientComponents />
+        </section>
+      </main>
+    )
   }
-
-  return (
-    <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
-      <NavBar />
-
-      <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-        <ClientComponents />
-      </section>
-    </main>
-  )
 }
