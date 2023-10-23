74327e6280425e8908b5d2575a781c709564b0a3
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-23 15:11:13 -0400

    Trying getSession in dashboard client components

--- src/app/components/dashClientComponents.tsx
+++ src/app/components/dashClientComponents.tsx
@@ -1,15 +1,26 @@
 'use client'
-import { useState } from 'react'
+import { getSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import { redirect } from 'next/navigation'
 import DashMenu from '@components/dashMenu'
 import SavedCoins from '@components/dashSavedCoins'
 import CryptoCardGrid from '@components/cryptoCardGrid'
 
+
 export default function ClientComponents() {
+  const session = getSession()
   const [activeDatatype, setActiveDatatype] = useState<string>('saved-coins')
+
+  useEffect(() => {
+    if(!session) {
+      redirect('/')
+    }
+  }, [session])
 
   const handleActiveDatatypeChange = (datatype: string) => {
     setActiveDatatype(datatype)
   }
+  
 
   return (
     <>
