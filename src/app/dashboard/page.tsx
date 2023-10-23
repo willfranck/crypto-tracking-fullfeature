d6e3f49c0d069d2c3ae664ad3f846f030408eae0
willfranck <43352447+willfranck@users.noreply.github.com>
2023-10-23 14:56:31 -0400

    Fix: Removed session code for now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,16 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@lib/auth'
-import { redirect } from 'next/navigation'
 import NavBar from '@components/navBar'
 import ClientComponents from '@components/dashClientComponents'
 
 
 export default function UserProfile() {
-  const session = getServerSession(authOptions)
+  return (
+    <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
+      <NavBar />
 
-  if (!session) {
-    redirect('/signin')
-  
-  } else {
-    return (
-      <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
-        <NavBar />
-
-        <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-          <ClientComponents />
-        </section>
-      </main>
-    )
-  }
+      <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
+        <ClientComponents />
+      </section>
+    </main>
+  )
 }
+
