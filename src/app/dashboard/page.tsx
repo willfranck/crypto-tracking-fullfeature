e7a5506ec8199317bc45481cc89afe8c8f34cd15
willfranck <43352447+willfranck@users.noreply.github.com>
2023-11-02 14:20:27 -0400

    Fix: Wrapped session around components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,33 +17,35 @@ export default function UserProfile() {
 
   useEffect(() => {
     const fetchSession = setInterval(() => update(), 1000 * 60)
-
-    if (!session) {
-      signOut()
-    }
-
     return () => clearInterval(fetchSession)
   }, [session, update])
 
 
   return (
     <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
-      <NavBar />
+      {session ? (
+        <>
+          <NavBar />
 
-      <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-        <DashMenu 
-          activeDatatypeChange={handleActiveDatatypeChange}
-        />
+          <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
+            <DashMenu 
+              activeDatatypeChange={handleActiveDatatypeChange}
+            />
 
-        {activeDatatype === 'saved-coins' ? (
-          <SavedCoins />
+            {activeDatatype === 'saved-coins' ? (
+              <SavedCoins />
 
-          ) : activeDatatype === 'search-coins' ? (
-            <CryptoCardGrid />
+              ) : activeDatatype === 'search-coins' ? (
+                <CryptoCardGrid />
 
-          ) : null
-        }
-      </section>
+              ) : null
+            }
+          </section>
+        </>
+        ) : (
+          signOut()
+        )
+      }
     </main>
   )
 }
