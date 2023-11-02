db72a322f27da9334a3fd81e25de2e51c0621799
willfranck <43352447+willfranck@users.noreply.github.com>
2023-11-02 15:57:27 -0400

    Fix: Added status to session data and useEffect

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import CryptoCardGrid from '@components/cryptoCardGrid'
 
 
 export default function UserProfile() {
-  const { data: session, update } = useSession()
+  const { data: session, status, update } = useSession()
   const [activeDatatype, setActiveDatatype] = useState<string>('saved-coins')
 
   const handleActiveDatatypeChange = (datatype: string) => {
@@ -17,35 +17,34 @@ export default function UserProfile() {
 
   useEffect(() => {
     const fetchSession = setInterval(() => update(), 1000 * 60)
+
+    if (status === 'unauthenticated') {
+      signOut()
+    }
+
     return () => clearInterval(fetchSession)
-  }, [session, update])
+  }, [update, status])
 
 
   return (
     <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
-      {session ? (
-        <>
-          <NavBar />
+      <NavBar />
 
-          <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-            <DashMenu 
-              activeDatatypeChange={handleActiveDatatypeChange}
-            />
+        <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
+          <DashMenu 
+            activeDatatypeChange={handleActiveDatatypeChange}
+          />
+          
+          {activeDatatype === 'saved-coins' ? (
+            <SavedCoins />
 
-            {activeDatatype === 'saved-coins' ? (
-              <SavedCoins />
+            ) : activeDatatype === 'search-coins' ? (
+              <CryptoCardGrid />
 
-              ) : activeDatatype === 'search-coins' ? (
-                <CryptoCardGrid />
-
-              ) : null
-            }
-          </section>
-        </>
-        ) : (
-          signOut()
-        )
-      }
+            ) : null
+          }
+        </section>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </main>
   )
 }
