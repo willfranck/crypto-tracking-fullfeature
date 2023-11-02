31a58afdb7ab282f552ca2aceabb96f90640379d
willfranck <43352447+willfranck@users.noreply.github.com>
2023-11-02 14:04:37 -0400

    Build: Added new session expiry handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,48 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { useSession, signOut } from 'next-auth/react'
 import NavBar from '@components/navBar'
-import DashClientComponents from '@components/dashClientComponents'
+import DashMenu from '@components/dashMenu'
+import SavedCoins from '@components/dashSavedCoins'
+import CryptoCardGrid from '@components/cryptoCardGrid'
 
 
 export default function UserProfile() {
+  const { data: session } = useSession()
+  const [activeDatatype, setActiveDatatype] = useState<string>('saved-coins')
+
+  const handleActiveDatatypeChange = (datatype: string) => {
+    setActiveDatatype(datatype)
+  }
+
+  useEffect(() => {
+    const fetchSession = setInterval(() => session, 1000 * 60)
+
+    if (!session) {
+      signOut()
+    }
+
+    return () => clearInterval(fetchSession)
+  }, [session])
+
+
   return (
     <main className='relative flex flex-col items-center w-full max-w-page min-h-screen mx-auto bg-dash bg-fixed bg-top isolate before:absolute before:content-{""} before:inset-0 before:bg-tintBlack before:-z-10'>
       <NavBar />
 
       <section className='flex flex-col flex-1 lg:flex-row items-center lg:items-start w-11/12 sm:w-5/6'>
-        <DashClientComponents />
+        <DashMenu 
+          activeDatatypeChange={handleActiveDatatypeChange}
+        />
+
+        {activeDatatype === 'saved-coins' ? (
+          <SavedCoins />
+
+          ) : activeDatatype === 'search-coins' ? (
+            <CryptoCardGrid />
+
+          ) : null
+        }
       </section>
     </main>
   )
