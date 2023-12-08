545fa46f33fae04e833b40f1cb98999d034a30f7
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-08 14:48:56 -0500

    Updated scope

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -66,7 +66,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
-          scope: 'openid email',
+          scope: 'openid https://www.googleapis.com/auth/userinfo.profile',
         },
       },
 
