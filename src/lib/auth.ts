dc153f934fe5ea1fc16f19395baf97d547568d59
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-08 14:13:39 -0500

    Added scope to Google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -64,6 +64,11 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: 'email',
+        },
+      },
 
       profile(profile) {
         return {
